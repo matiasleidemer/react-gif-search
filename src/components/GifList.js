95b2a0fc8a04c1050016cce1a2f8082088d074c9
Matias Henrique Leidemer <matiasleidemer@gmail.com>
2016-09-21 00:01:08 -0300

    first commit

---
+++ src/components/GifList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GifItem from './GifItem';
+
+const GifList = (props) => {
+  const gifItems = props.gifs.map((image) => {
+    return <GifItem key={image.id}
+                    gif={image}
+                    onGifSelect={props.onGifSelect} />
+  });
+
+  return (
+    <ul className="gif-list">{gifItems}</ul>
+  );
+};
+
+export default GifList;
